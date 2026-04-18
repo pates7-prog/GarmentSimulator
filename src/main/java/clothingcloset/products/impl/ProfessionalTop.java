c49011fb113457f9d5fd3de004657d16f1ea5db3
shilpatel <pates7@farmingdale.edu>
2026-04-17 21:47:31 -0400

    this for the pants,top,shoes.java

--- src/main/java/clothingcloset/products/impl/ProfessionalTop.java
+++ src/main/java/clothingcloset/products/impl/ProfessionalTop.java
@@ -6,5 +6,3 @@ public class ProfessionalTop implements Top {
     public void wear() { System.out.println("Wearing a professional top"); }
 }
 
-// abstract factoru
-//to make a group of similar clothes or the idea of clothes
