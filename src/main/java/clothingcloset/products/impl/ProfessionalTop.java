14e04119047c61209f4491c7da19f536459aea7e
shilpatel <pates7@farmingdale.edu>
2026-04-17 21:46:26 -0400

    abstract

--- src/main/java/clothingcloset/products/impl/ProfessionalTop.java
+++ src/main/java/clothingcloset/products/impl/ProfessionalTop.java
@@ -5,3 +5,6 @@ import clothingcloset.products.Top;
 public class ProfessionalTop implements Top {
     public void wear() { System.out.println("Wearing a professional top"); }
 }
+
+// abstract factoru
+//to make a group of similar clothes or the idea of clothes
